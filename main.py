2f68fec8f523925a120df304deca290f827fad42
XlynxX <kirja0821@gmail.com>
2020-11-23 08:21:26 +0200

    'main-file-update'

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PySide2.QtMultimedia import QSound
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import *
+from locale import getdefaultlocale
 import MCPP
 import sys
 import time
@@ -10,15 +11,12 @@ import time
 class Worker(QRunnable):
     '''
     Worker thread
-
     Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-
     :param callback: The function callback to run on this worker thread. Supplied args and
                      kwargs will be passed through to the runner.
     :type callback: function
     :param args: Arguments to pass to the callback function
     :param kwargs: Keywords to pass to the callback function
-
     '''
 
     def __init__(self, fn, *args, **kwargs):
@@ -60,15 +58,25 @@ class MainScreen(QtWidgets.QMainWindow, MCPP.Ui_MainWindow):
             # Вызов функции проигрывания звука при нажатии
             child.clicked.connect(self.Play)
         
-        self.pushButton1.clicked.connect(self.en)  # Выполнить функцию test при нажатии
+        self.langButton.clicked.connect(self.retranslate)  # Выполнить функцию test при нажатии
         self.threadpool = QThreadPool() # Это нужно для создания потока
         print("Доступно потоков %d " % self.threadpool.maxThreadCount()) # Доступное количество потоков
-        
-        # Встраивание текста с переводом
+
+    # Встраивание текста с переводом
+    def retranslate(self):
+        translator = QtCore.QTranslator(self)
+        translator.load(':/lang/lang/en_US')
+        print(translator.load)
+        QApplication.installTranslator(translator)
+
         self.label1.setText(QApplication.translate('app', 'Открыть  ресурспак'))
         self.label2.setText(QApplication.translate('app', 'Модификации'))
         self.label3.setText(QApplication.translate('app', 'Настройки...'))
         self.label4.setText(QApplication.translate('app', 'Конвертировать'))
+
+        if not QApplication.installTranslator(translator):
+            print("Can not install translation!")
+    # Встраивание текста с переводом
     
     def thread_test(self):
         print('Работает!')
@@ -83,30 +91,30 @@ class MainScreen(QtWidgets.QMainWindow, MCPP.Ui_MainWindow):
     def Play(self):
         QSound.play(":/sounds/res/click.wav")
 
-    def en(self):
+    #def en(self):
         #create a new translator and load the desired translation file
-        translator = QtCore.QTranslator(app)
-        translator.load(":/lang/lang/en_US")
+        #translator = QtCore.QTranslator(app)
+        #translator.load(":/lang/lang/en_US")
         #install translator to the app 
         #app is the variable created in (if __name__ == "__main__":) section, make it gloabl
-        app.installTranslator(translator)
+        #app.installTranslator(translator)
         #call retranslateUi on ui, which is defined in the same section as app
         #MainWindow : created in the same section 
         #self.retranslateUi(QtWidgets.QMainWindow())
-        self.label1.setText(QApplication.translate('app', 'Открыть  ресурспак'))
-        self.label2.setText(QApplication.translate('app', 'Модификации'))
-        self.label3.setText(QApplication.translate('app', 'Настройки...'))
-        self.label4.setText(QApplication.translate('app', 'Конвертировать'))
-        if not app.installTranslator(translator):
-            print("Can not install translation!")
+        #self.label1.setText(QApplication.translate('app', 'Открыть  ресурспак'))
+        #self.label2.setText(QApplication.translate('app', 'Модификации'))
+        #self.label3.setText(QApplication.translate('app', 'Настройки...'))
+        #self.label4.setText(QApplication.translate('app', 'Конвертировать'))
+        #if not app.installTranslator(translator):
+            #print("Can not install translation!")
 
 def main():
-    global app
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
     
     # Загрузка перевода
+    locale = getdefaultlocale()
     translator = QtCore.QTranslator(app)
-    translator.load(':/lang/lang/ru_US')
+    translator.load(':/lang/lang/%s' % locale[0])
     app.installTranslator(translator)
     if not app.installTranslator(translator):
         print("Can not install translation!")
